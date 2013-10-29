e4672dfe5a6244a6a589d228bd5a3b577031842f
get install rails <hsure@ncsu.edu>
2013-10-29 00:16:07 -0400

    changed submitted_content_controller and helper files

--- app/models/assignment_participant.rb
+++ app/models/assignment_participant.rb
@@ -189,7 +189,7 @@ class AssignmentParticipant < Participant
   # Appends the hyperlink to a list that is stored in YAML format in the DB
   # @exception  If is hyperlink was already there
   #             If it is an invalid URL
-  def submmit_hyperlink(hyperlink)
+  def submit_hyperlink(hyperlink)
     hyperlink.strip!
     raise "The hyperlink cannot be empty" if hyperlink.empty?
 
